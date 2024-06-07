75ab2b1e4aab6ad440d6e47cfe8436ec7a5145df
ZyerTCoder <44621908+ZyerTCoder@users.noreply.github.com>
2024-06-07 19:50:12 +0100

    this handler is incorrect and isn't doing anything here

--- src/analysis/retail/warlock/demonology/modules/resources/SoulShardTracker.tsx
+++ src/analysis/retail/warlock/demonology/modules/resources/SoulShardTracker.tsx
@@ -1,4 +1,3 @@
-import SPELLS from 'common/SPELLS';
 import RESOURCE_TYPES from 'game/RESOURCE_TYPES';
 import { Options } from 'parser/core/Analyzer';
 import { CastEvent } from 'parser/core/Events';
@@ -18,18 +17,6 @@ class SoulShardTracker extends ResourceTracker {
       classResources.max /= 10;
       super.onCast(event);
     }
-  }
-
-  getAdjustedCost(event: CastEvent) {
-    let cost = super.getAdjustedCost(event) ?? 0;
-    // Demonic Calling (T30 talent) proc reduces the cost of next Call Dreadstalkers by 1 shard
-    if (
-      event.ability.guid === SPELLS.CALL_DREADSTALKERS.id &&
-      this.selectedCombatant.hasBuff(SPELLS.DEMONIC_CALLING_BUFF.id)
-    ) {
-      cost -= 1;
-    }
-    return cost;
   }
 }
 
