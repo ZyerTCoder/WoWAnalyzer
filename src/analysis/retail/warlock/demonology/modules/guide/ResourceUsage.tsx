36abc9af0f78aff5e9c2446611532cfc233a6414
ZyerTCoder <44621908+ZyerTCoder@users.noreply.github.com>
2024-07-16 12:49:58 +0100

    start shard tracker

--- src/analysis/retail/warlock/demonology/modules/guide/ResourceUsage.tsx
+++ src/analysis/retail/warlock/demonology/modules/guide/ResourceUsage.tsx
@@ -1,5 +1,6 @@
 import { GuideProps, Section, SubSection } from 'interface/guide';
 import CombatLogParser from '../../CombatLogParser';
+import ResourceTracker from 'parser/shared/modules/resources/resourcetracker/ResourceTracker';
 
 function ResourceUsage({ modules }: GuideProps<typeof CombatLogParser>) {
   return (
@@ -12,8 +13,26 @@ function ResourceUsage({ modules }: GuideProps<typeof CombatLogParser>) {
           {modules.soulshardGraph.plot}
         </>
       </SubSection>
+      <SubSection>
+        {console.error(modules.soulShardTracker.buildersObj)}
+        <table>
+          <tbody>{getWastedShards(modules.soulShardTracker)}</tbody>
+        </table>
+      </SubSection>
     </Section>
   );
+}
+
+function getWastedShards(tracker: ResourceTracker) {
+  const builders = tracker.buildersObj;
+  return Object.keys(builders)
+    .filter((key) => builders[Number(key)].wasted > 0)
+    .map((key) => (
+      <tr key={key}>
+        <td>{key}</td>
+        <td>{builders[Number(key)].wasted} shards wasted</td>
+      </tr>
+    ));
 }
 
 export default ResourceUsage;
